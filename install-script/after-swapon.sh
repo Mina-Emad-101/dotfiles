1ddd73d22c85693802ddb7315de1c50bd3a336ac
Mina Emad <menamanos@gmail.com>
2025-03-21 11:31:11 +0000

    fix

--- install-script/after-swapon.sh
+++ install-script/after-swapon.sh
@@ -2,10 +2,13 @@
 
 pacstrap -K /mnt base linux-headers linux-firmware base-devel networkmanager sof-firmware vim bash-completion os-prober git;
 genfstab -U /mnt >> /mnt/etc/fstab;
-cp $SCRIPT_DIR/after-chroot.sh /mnt;
-cp $SCRIPT_DIR/after-reboot.sh /mnt;
-cp $SCRIPT_DIR/after-reboot2.sh /mnt;
+SCRIPT_DIR="$(dirname "$0")";
+echo $SCRIPT_DIR;
+cp "$SCRIPT_DIR/after-chroot.sh" /mnt;
+cp "$SCRIPT_DIR/after-reboot.sh" /mnt;
+cp "$SCRIPT_DIR/after-reboot2.sh" /mnt;
 arch-chroot /mnt /after-chroot.sh;
+
 echo " ------------------------------ ";
 echo "| You can now Reboot           |";
 echo "| then run /after-reboot.sh    |";
