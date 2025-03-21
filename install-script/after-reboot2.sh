a32e312f2beb2069053144040f0781b2c0976d28
Mina Emad <menamanos@gmail.com>
2025-03-22 00:02:48 +0200

    gitignore refresh

--- install-script/after-reboot2.sh
+++ install-script/after-reboot2.sh
@@ -6,8 +6,10 @@ git config --global user.name "Mina Emad";
 git config --global user.email "menamanos@gmail.com";
 git config --global pull.rebase true;
 git config --global credentials.helper store;
+git config --global init.defaultBranch main;
 
 git clone https://github.com/mina-emad-101/dotfiles.git;
+rm -rf dotfiles/tmux/.config/tmux/plugins;
 
 rm -rf $HOME/.bashrc;
 cd dotfiles;
@@ -40,18 +42,16 @@ echo '        Option "FingerHigh" "50"' >> touchpad-settings;
 echo '        Option "MaxTapTime" "125"' >> touchpad-settings;
 echo 'EndSection' >> touchpad-settings;
 
-sudo cat touchpad-settings >> /etc/X11/xorg.conf.d/70-synaptics.conf;
+sudo /bin/sh -c 'cat touchpad-settings >> /etc/X11/xorg.conf.d/70-synaptics.conf;';
 rm -rf touchpad-settings;
 
 sudo sed -i "92,93 s/#\s*//" /etc/pacman.conf;
 sudo pacman -Sy;
 
-sudo pacman -S xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 alacritty rofi pipewire multilib/lib32-pipewire pipewire-pulse pavucontrol dunst picom tmux yazi neovim firefox ttf-jetbrains-mono-nerd starship fzf ripgrep caja lxsession-gtk3 lxappearance lazygit pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse vlc mariadb redis pamixer xclip php php-apache php-fpm php-gd php-imagick php-redis php-sodium php-sqlite composer;
+sudo pacman -S xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 alacritty rofi pipewire multilib/lib32-pipewire pipewire-pulse pavucontrol dunst picom tmux yazi neovim firefox ttf-jetbrains-mono-nerd starship fzf ripgrep caja lxsession-gtk3 lxappearance lazygit pipewire pipewire-alsa pipewire-audio pipewire-jack pipewire-pulse vlc mariadb redis pamixer xclip php php-apache php-fpm php-gd php-imagick php-redis php-sodium php-sqlite composer nodejs npm;
 sudo systemctl enable lightdm.service;
 
-sudo mkdir -p /usr/share/themes/Dracula;
-sudo cp -r .themes/Dracula /usr/share/themes/Dracula
-sudo mkdir -p /usr/share/icons/Dracula;
-sudo cp -r .icons/Dracula /usr/share/icons/Dracula;
+sudo cp -r .themes/Dracula /usr/share/themes/
+sudo cp -r .icons/Dracula /usr/share/icons/;
 
 sudo rm -rf /after-reboot2.sh;
