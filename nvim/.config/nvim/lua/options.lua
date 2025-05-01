7394efe2b806d7054345ee8d668f32eb29f6dc3f
Mina Emad <menamanos@gmail.com>
2025-05-01 04:42:10 +0300

    tabs indentation = 2 for javascript and html

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -11,10 +11,12 @@ vim.o.undodir = vim.fn.expand("~/.config/nvim/undofiles")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 2
+vim.opt.shiftwidth = 4
 -- vim.opt.expandtab = true
+
+vim.opt.autoindent = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -22,8 +24,6 @@ vim.opt.incsearch = true
 vim.opt.termguicolors = true
 
 vim.o.clipboard = "unnamedplus"
-
-vim.opt.autoindent = true
 
 vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
@@ -37,3 +37,11 @@ vim.opt.foldmethod = "manual"
 -- vim.opt.foldmethod = 'expr'
 -- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldlevelstart = 99
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "javascript,js,html,blade,blade.php",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+	end
+})
