613d66d46ba5d6de6deb87903cb92860b6a27cbe
Mina Emad <menamanos@gmail.com>
2026-02-27 19:48:09 +0200

    fix

--- scripts/ntfs-fix.sh
+++ scripts/ntfs-fix.sh
@@ -3,7 +3,7 @@
 NAME=$(echo "$1" | awk -F"/" '{print $NF}');
 
 ntfsfix "$1";
-umount "/run/media/$NAME";
-rm -rf "/run/media/$NAME";
-mkdir "/run/media/$NAME";
-ntfs-3g "$1" "/run/media/$NAME";
+umount "/run/media/mina/$NAME";
+rm -rf "/run/media/mina/$NAME";
+mkdir "/run/media/mina/$NAME";
+ntfs-3g "$1" "/run/media/mina/$NAME";
