dbff3f565bb1c7bf74b86c826e319adb1433d592
Mina Emad <menamanos@gmail.com>
2026-04-18 14:13:43 +0200

    disable laravel.nvim

--- nvim/.config/nvim/lua/plugins/laravel.lua
+++ nvim/.config/nvim/lua/plugins/laravel.lua
@@ -1,23 +1,23 @@
 return {
-	"adalessa/laravel.nvim",
-	dependencies = {
-		"kevinhwang91/promise-async",
-		"nvim-telescope/telescope.nvim",
-		"tpope/vim-dotenv",
-		"MunifTanjim/nui.nvim",
-		"nvimtools/none-ls.nvim",
-		"nvim-neotest/nvim-nio",
-	},
-	cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-	keys = {
-		{ "<leader>la", ":Laravel artisan<cr>" },
-		{ "<leader>lr", ":Laravel routes<cr>" },
-		{ "<leader>lm", ":Laravel related<cr>" },
-	},
-	event = { "VeryLazy" },
-	config = true,
-	cond = function()
-	  return vim.fn.filereadable("artisan") == 1
-		  or vim.fn.filereadable("composer.json") == 1
-	end,
+	-- "adalessa/laravel.nvim",
+	-- dependencies = {
+	-- 	"kevinhwang91/promise-async",
+	-- 	"nvim-telescope/telescope.nvim",
+	-- 	"tpope/vim-dotenv",
+	-- 	"MunifTanjim/nui.nvim",
+	-- 	"nvimtools/none-ls.nvim",
+	-- 	"nvim-neotest/nvim-nio",
+	-- },
+	-- cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+	-- keys = {
+	-- 	{ "<leader>la", ":Laravel artisan<cr>" },
+	-- 	{ "<leader>lr", ":Laravel routes<cr>" },
+	-- 	{ "<leader>lm", ":Laravel related<cr>" },
+	-- },
+	-- event = { "VeryLazy" },
+	-- config = true,
+	-- cond = function()
+	--   return vim.fn.filereadable("artisan") == 1
+	-- 	  or vim.fn.filereadable("composer.json") == 1
+	-- end,
 }
