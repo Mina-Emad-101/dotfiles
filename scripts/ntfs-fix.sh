0376d4a37814f1eeac8a74789332c7f342b15abb
Mina Emad <menamanos@gmail.com>
2026-06-05 10:54:00 +0300

    ntfs fix

--- scripts/ntfs-fix.sh
+++ scripts/ntfs-fix.sh
@@ -5,5 +5,5 @@ NAME=$(blkid | grep "$1" | grep -o 'LABEL="[^"]*"' | grep -o '".*"' | sed 's/"//
 ntfsfix "$1";
 umount "/run/media/mina/$NAME";
 rm -rf "/run/media/mina/$NAME";
-mkdir "/run/media/mina/$NAME";
+mkdir -p "/run/media/mina/$NAME";
 ntfs-3g "$1" "/run/media/mina/$NAME";
